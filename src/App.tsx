f50e8fc0a2188a39ccd82eec0d4b8f63928c7a57
inodw1 <inodwagachchi@gmail.com>
2023-12-17 11:48:04 +0530

    Build a wrapper button component to render different elements

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,25 @@
 import React from "react";
 import Input from "./component/input.tsx";
+import Button from "./component/button.tsx";
 
 function App() {
     return (
         <main>
             <Input id="name" label="Your name" type="text" />
             <Input id="age" label="Your age" type="number" />
+
+            <p>
+                <Button el="button">A Button</Button>
+            </p>
+            <p>
+                <Button
+                    el="anchor"
+                    href="https://inod-wagachchi.netlify.app/"
+                    target="blank"
+                >
+                    A link
+                </Button>
+            </p>
         </main>
     );
 }
